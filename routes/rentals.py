d88aed23e7f4cc165bfd16bd9c9dd7e83203ea59
Guy Braylovski <guybrylov@Gmail.com>
2026-04-05 21:23:41 +0300

    rental first half done

--- routes/rentals.py
+++ routes/rentals.py
@@ -14,7 +14,7 @@ def init_rentals_routes(app):
         filtered_rental = []
         for item in rental_data:
 
-            item_status_str = item['status'].name.lower() # Gets 'active' or 'returned'
+            item_status_str = item['status'].name.lower()
             match_status = (selected_status == 'all' or item_status_str == selected_status)
 
             customer = next((c for c in customer_data if c['id'] == item['customer_id']), None)
@@ -22,6 +22,7 @@ def init_rentals_routes(app):
 
             equipment = next((e for e in equipment_data if e['id'] == item['equipment_id']), None)
             item['equipment_name'] = equipment['name'] if equipment else "Unknown Equipment"
+
             
             if match_status:
                 filtered_rental.append(item)
@@ -30,7 +31,27 @@ def init_rentals_routes(app):
         return render_template(
             'rentals/list.html', 
             rental=filtered_rental,     # The filtered list
-            customer=customer_data,
             statuses=unique_statuses,     # The dynamic dropdown list
             selected_status=selected_status, # To keep the dropdown selected
     )
+
+    @app.route('/rentals/<int:rental_id>/return')
+    def rental_return(rental_id):       
+        global rental_data
+
+        for index, r  in enumerate(rental_data):
+            if r['id'] == rental_id:
+                if r['status'] == STATUS.RETURNED:
+                    flash("Can't return a returned item", "danger")
+                    return redirect('/rentals')
+                rental_data[index]['status'] = STATUS.RETURNED
+                flash("Item Change to returned", "success")
+                return redirect('/rentals')
+        flash("Error occured couldn't change status", "danger")
+        return redirect('/rentals')
+        
+                
+        
+
+        
+    
